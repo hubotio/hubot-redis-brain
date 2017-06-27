088dc4cba44246e730ce1158c274a1239de9eef8
Alex Feyerke <espy@users.noreply.github.com>
2017-06-27 17:12:20 +0200

    chore: remove deprecated code

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ const path = require('path')
 
 module.exports = function (robot, scripts) {
   const scriptsPath = path.resolve(__dirname, 'src')
-  return fs.exists(scriptsPath, function (exists) {
-    if (exists) {
-      return (() => {
-        const result = []
-        for (let script of fs.readdirSync(scriptsPath)) {
-          if ((scripts != null) && !scripts.includes('*')) {
-            if (scripts.includes(script)) { result.push(robot.loadFile(scriptsPath, script)) } else {
-              result.push(undefined)
-            }
-          } else {
-            result.push(robot.loadFile(scriptsPath, script))
+  if (isDirSync(scriptsPath)) {
+    return (() => {
+      const result = []
+      for (let script of fs.readdirSync(scriptsPath)) {
+        if ((scripts != null) && !scripts.includes('*')) {
+          if (scripts.includes(script)) { result.push(robot.loadFile(scriptsPath, script)) } else {
+            result.push(undefined)
           }
+        } else {
+          result.push(robot.loadFile(scriptsPath, script))
         }
-        return result
-      })()
+      }
+      return result
+    })()
+  }
+}
+
+function isDirSync (aPath) {
+  try {
+    return fs.statSync(aPath).isDirectory()
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      return false
+    } else {
+      throw e
     }
-  })
+  }
 }
