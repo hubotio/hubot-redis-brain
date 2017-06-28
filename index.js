32165a4857c4fd727a76d1aa1f2b2e9713b06557
Gregor <gregor@martynus.net>
2017-06-27 18:41:36 -0700

    refactor: simplifications

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-const fs = require('fs')
+'use strict'
+
 const path = require('path')
 
 module.exports = function (robot, scripts) {
   const scriptsPath = path.resolve(__dirname, 'src')
-  if (isDirSync(scriptsPath)) {
-    return (() => {
-      const result = []
-      for (let script of fs.readdirSync(scriptsPath)) {
-        if ((scripts != null) && !scripts.includes('*')) {
-          if (scripts.includes(script)) { result.push(robot.loadFile(scriptsPath, script)) } else {
-            result.push(undefined)
-          }
-        } else {
-          result.push(robot.loadFile(scriptsPath, script))
-        }
-      }
-      return result
-    })()
-  }
-}
-
-function isDirSync (aPath) {
-  try {
-    return fs.statSync(aPath).isDirectory()
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      return false
-    } else {
-      throw e
-    }
-  }
+  robot.loadFile(scriptsPath, 'redis-brain.js')
 }
--- src/redis-brain.js
+++ src/redis-brain.js
@@ -17,44 +17,30 @@ const Url = require('url')
 const Redis = require('redis')
 
 module.exports = function (robot) {
-  let client, prefix, redisUrlEnv
-  const redisUrl = (() => {
-    if (process.env.REDISTOGO_URL != null) {
-      redisUrlEnv = 'REDISTOGO_URL'
-      return process.env.REDISTOGO_URL
-    } else if (process.env.REDISCLOUD_URL != null) {
-      redisUrlEnv = 'REDISCLOUD_URL'
-      return process.env.REDISCLOUD_URL
-    } else if (process.env.BOXEN_REDIS_URL != null) {
-      redisUrlEnv = 'BOXEN_REDIS_URL'
-      return process.env.BOXEN_REDIS_URL
-    } else if (process.env.REDIS_URL != null) {
-      redisUrlEnv = 'REDIS_URL'
-      return process.env.REDIS_URL
-    } else {
-      return 'redis://localhost:6379'
-    }
-  })()
+  let client, prefix
+  const redisUrlEnv = getRedisEnv()
+  const redisUrl = process.env[redisUrlEnv] || 'redis://localhost:6379'
 
-  if (redisUrlEnv != null) {
+  if (redisUrlEnv) {
     robot.logger.info(`hubot-redis-brain: Discovered redis from ${redisUrlEnv} environment variable`)
   } else {
     robot.logger.info('hubot-redis-brain: Using default redis on localhost:6379')
   }
 
-  if (process.env.REDIS_NO_CHECK != null) {
+  if (process.env.REDIS_NO_CHECK) {
     robot.logger.info('Turning off redis ready checks')
   }
 
   const info = Url.parse(redisUrl, true)
+
   if (info.hostname === '') {
     client = Redis.createClient(info.pathname)
-    prefix = (info.query != null ? info.query.toString() : undefined) || 'hubot'
+    prefix = (info.query ? info.query.toString() : undefined) || 'hubot'
   } else {
-    client = info.auth || (process.env.REDIS_NO_CHECK != null)
+    client = (info.auth || process.env.REDIS_NO_CHECK)
               ? Redis.createClient(info.port, info.hostname, {no_ready_check: true})
             : Redis.createClient(info.port, info.hostname)
-    prefix = (info.path != null ? info.path.replace('/', '') : undefined) || 'hubot'
+    prefix = (info.path ? info.path.replace('/', '') : undefined) || 'hubot'
   }
 
   robot.brain.setAutoSave(false)
@@ -73,17 +59,17 @@ module.exports = function (robot) {
         robot.brain.emit('connected')
       }
 
-      return robot.brain.setAutoSave(true)
+      robot.brain.setAutoSave(true)
     })
 
   if (info.auth) {
     client.auth(info.auth.split(':')[1], function (err) {
       if (err) {
         return robot.logger.error('hubot-redis-brain: Failed to authenticate to Redis')
-      } else {
-        robot.logger.info('hubot-redis-brain: Successfully authenticated to Redis')
-        return getData()
       }
+
+      robot.logger.info('hubot-redis-brain: Successfully authenticated to Redis')
+      getData()
     })
   }
 
@@ -91,13 +77,13 @@ module.exports = function (robot) {
     if (/ECONNREFUSED/.test(err.message)) {
 
     } else {
-      return robot.logger.error(err.stack)
+      robot.logger.error(err.stack)
     }
   })
 
   client.on('connect', function () {
     robot.logger.debug('hubot-redis-brain: Successfully connected to Redis')
-    if (!info.auth) { return getData() }
+    if (!info.auth) { getData() }
   })
 
   robot.brain.on('save', (data) => {
@@ -107,5 +93,23 @@ module.exports = function (robot) {
     client.set(`${prefix}:storage`, JSON.stringify(data))
   })
 
-  return robot.brain.on('close', () => client.quit())
+  robot.brain.on('close', () => client.quit())
+}
+
+function getRedisEnv () {
+  if (process.env.REDISTOGO_URL) {
+    return 'REDISTOGO_URL'
+  }
+
+  if (process.env.REDISCLOUD_URL) {
+    return 'REDISCLOUD_URL'
+  }
+
+  if (process.env.BOXEN_REDIS_URL) {
+    return 'BOXEN_REDIS_URL'
+  }
+
+  if (process.env.REDIS_URL) {
+    return 'REDIS_URL'
+  }
 }
