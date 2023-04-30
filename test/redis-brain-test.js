25d0249e080c881509d17b74f8c67083703de866
Joey Guerra <github@joeyguerra.com>
2023-04-30 15:53:53 -0500

    chore(pacakge): Update dependencies

--- test/redis-brain-test.js
+++ test/redis-brain-test.js
@@ -2,29 +2,17 @@
 
 /* global describe, it */
 /* eslint-disable no-unused-expressions */
+const Redis = require('redis')
+const shell = require('hubot/src/adapters/shell')
+const Adapter = require('hubot/src/adapter')
 
 const path = require('path')
 
 const chai = require('chai')
 const sinon = require('sinon')
 const Hubot = require('hubot')
-const proxyquire = require('proxyquire').noCallThru()
 
 const expect = chai.expect
-const RedisClientMock = {
-  get: function () {},
-  set: function () {},
-  on: function () {},
-  auth: function () {},
-  quit: function () {}
-}
-const RedisMock = {
-  createClient: () => RedisClientMock
-}
-proxyquire('../src/redis-brain', {
-  redis: RedisMock
-})
-
 const Robot = Hubot.Robot
 
 chai.use(require('sinon-chai'))
@@ -35,16 +23,24 @@ describe('redis-brain', () => {
   })
 
   it('connects to redis', () => {
-    const robot = new Robot(null, 'mock-adapter-v3', false, 'hubot')
+    // The mock-adapter-v3 has old dependencies with security issues, so don't use that.
+    // Instead, we'll use the shell adapter and mock the use() method because the current version of Hubot
+    // doesn't have a way to specify an adapter via a path.
 
-    sinon.spy(RedisMock, 'createClient')
+    shell.use = robot => {
+      return new Adapter()
+    }
+
+    const robot = new Robot(null, 'shell', false, 'hubot')
+
+    sinon.spy(Redis, 'createClient')
     sinon.spy(robot.logger, 'info')
 
     robot.loadFile(path.resolve('src/'), 'redis-brain.js')
     robot.run()
 
-    expect(RedisMock.createClient).to.have.been.calledOnce
-    expect(RedisMock.createClient).to.have.been.calledWith('6379', 'localhost')
+    expect(Redis.createClient).to.have.been.calledOnce
+    expect(Redis.createClient).to.have.been.calledWith('6379', 'localhost')
     expect(robot.logger.info).to.have.been.calledWith('hubot-redis-brain: Using default redis on localhost:6379')
 
     robot.shutdown()
