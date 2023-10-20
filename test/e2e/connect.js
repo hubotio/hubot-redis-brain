3c955a016b8db61de0ec4eb116add1517339e473
Joey Guerra <github@joeyguerra.com>
2023-10-19 21:57:44 -0500

    feat: Added redis url to robot.config (#62)
    * feat: Added redis url to robot.config
    
    * upgrading redis
    
    * make e2e test work

--- test/e2e/connect.js
+++ test/e2e/connect.js
@@ -1,39 +1,21 @@
 'use strict'
-
-/* global describe, it */
-/* eslint-disable no-unused-expressions */
-const shell = require('hubot/src/adapters/shell')
-const Adapter = require('hubot/src/adapter')
-
+const { describe, it } = require('node:test')
+const assert = require('node:assert/strict')
 const path = require('path')
-
-const chai = require('chai')
-const sinon = require('sinon')
-const Hubot = require('hubot')
-
-const expect = chai.expect
-const Robot = Hubot.Robot
-
-chai.use(require('sinon-chai'))
-
+const Robot = require('hubot/src/robot.js')
 describe('e2e', () => {
-  it('connects to redis', done => {
-    shell.use = robot => {
-      return new Adapter()
-    }
-
-    const robot = new Robot(null, 'shell', false, 'hubot')
-    sinon.spy(robot.logger, 'debug')
+  it('connects to redis', async () => {
+    const robot = new Robot('shell', false, 'hubot')
+    await robot.loadAdapter()
     robot.brain.on('loaded', actual => {
       const expected = { users: {}, _private: {} }
-      expect(actual).to.deep.equal(expected)
+      assert.deepEqual(actual, expected)
     })
     robot.brain.on('connected', () => {
-      expect(robot.logger.debug).to.have.been.calledWith('hubot-redis-brain: Successfully connected to Redis')
-      robot.shutdown()
-      done()
+      assert.ok(true)
     })
-    robot.loadFile(path.resolve('src/'), 'redis-brain.js')
-    robot.run()
+    await robot.loadFile(path.resolve('src/'), 'redis-brain.js')
+    await robot.run()
+    robot.shutdown()
   })
 })
