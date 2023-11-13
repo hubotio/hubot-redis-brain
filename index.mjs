6f07bf294da0b634685585b926fb8b85b7d48587
Joey Guerra <github@joeyguerra.com>
2023-11-13 13:52:22 -0600

    fix: make async/await

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-export default (robot) => {
+export default async (robot) => {
   const scriptsPath = path.resolve(__dirname, 'src')
-  robot.loadFile(scriptsPath, 'RedisBrain.mjs')
+  await robot.loadFile(scriptsPath, 'RedisBrain.mjs')
 }
